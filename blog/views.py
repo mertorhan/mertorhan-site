5364db4dc490630406c60788d08c0c38523379ea
mertorhan <m.orhanoruc@gmail.com>
2026-06-25 16:34:06 +0300

    Blog liste sayfası dinamiğe çevrildi (BlogPost for döngüsü)

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 
 def post_list(request):
-    return render(request, "blog/post_list.html")
+    posts = BlogPost.objects.all()
+    return render(request, "blog/post_list.html", {"posts": posts})
+
 
 def post_detail(request):
     return render(request, "blog/post_detail.html")
